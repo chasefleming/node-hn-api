158df6d8abf717cd849a0e654175f8646147d973
Chase Fleming <chaseefleming@gmail.com>
2014-10-16 20:59:29 -0700

    Refactor and bump version.

--- index.js
+++ index.js
@@ -1,64 +1,46 @@
-var request = require('request');
+var request = require('request'),
+    ITEM_URL = 'https://hacker-news.firebaseio.com/v0/item/{itemId}.json',
+    USER_URL = 'https://hacker-news.firebaseio.com/v0/user/{userName}.json',
+    TOP_STORIES_URL = 'https://hacker-news.firebaseio.com/v0/topstories.json',
+    MAX_ITEM_URL = 'https://hacker-news.firebaseio.com/v0/maxitem.json';
+
+
+function respond (callback) {
+  return function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      try {
+        var data = JSON.parse(body);
+      } catch (err) {
+        callback(err);
+      }
+      return callback(null, data);
+    }
+    return callback(error);
+  }
+}
 
 
 function getItem(itemId, callback) {
-  request('https://hacker-news.firebaseio.com/v0/item/' + itemId + '.json', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      try {
-        var data = JSON.parse(body);
-      } catch (err) {
-        callback(err);
-      }
-      return callback(null, data);
-    }
-    return callback(error);
-  });
+  request(ITEM_URL.replace('{itemId}', itemId), respond(callback));
 }
 
 function getUser(userName, callback) {
-  request('https://hacker-news.firebaseio.com/v0/user/' + userName + '.json', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      try {
-        var data = JSON.parse(body);
-      } catch (err) {
-        callback(err);
-      }
-      return callback(null, data);
-    }
-    return callback(error);
-  });
+  request(USER_URL.replace('{userName}', userName), respond(callback));
 }
 
 function getTopStories(callback) {
-  request('https://hacker-news.firebaseio.com/v0/topstories.json', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      try {
-        var data = JSON.parse(body);
-      } catch (err) {
-        callback(err);
-      }
-      return callback(null, data);
-    }
-    return callback(error);
-  });
+  request(TOP_STORIES_URL, respond(callback));
 }
 
 function getMaxItem(callback) {
-  request('https://hacker-news.firebaseio.com/v0/maxitem.json', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      try {
-        var data = JSON.parse(body);
-      } catch (err) {
-        callback(err);
-      }
-      return callback(null, data);
-    }
-    return callback(error);
-  });
+  request(MAX_ITEM_URL, respond(callback));
 }
 
 
-exports.item = getItem;
-exports.user = getUser;
-exports.topStories = getTopStories;
-exports.maxItem = getMaxItem;
+
+module.exports = {
+  item: getItem,
+  user: getUser,
+  topStories: getTopStories,
+  maxItem: getMaxItem
+}
